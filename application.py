5e410c7d603b7f6c1bce7915e71cfb108e202c7a
contamination-lab <andrea.mirino@gmail.com>
2021-01-11 21:54:02 +0100

    try except on socket

--- application.py
+++ application.py
@@ -39,5 +39,8 @@ def handle_my_custom_event1( json1 ):
 if __name__ == '__main__':
     app.config['PREFERRED_URL_SCHEME'] = 'https'   
     app.wsgi_app = ProxyFix(app.wsgi_app)   
-    socketio.run( app, port=8089, host='0.0.0.0', debug=True, keyfile='key.pem', certfile='cert.pem' )
+    try:
+        socketio.run( app, port=8089, host='0.0.0.0', debug=True, keyfile='key.pem', certfile='cert.pem' )
+    except:
+        pass #ignora l'errore
  
