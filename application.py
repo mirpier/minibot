0a59fefe6c346fb92a44c9ef3335b5b786044bb3
contamination-lab <andrea.mirino@gmail.com>
2021-01-12 14:19:00 +0100

    certificati

--- application.py
+++ application.py
@@ -39,5 +39,5 @@ def handle_my_custom_event1( json1 ):
 if __name__ == '__main__':
     app.config['PREFERRED_URL_SCHEME'] = 'https'   
     app.wsgi_app = ProxyFix(app.wsgi_app)   
-    socketio.run( app, port=8089, host='0.0.0.0', debug=True, keyfile='key.pem', certfile='cert.pem' )  
+    socketio.run( app, port=8089, host='0.0.0.0', debug=True, keyfile='cert.key', certfile='cert.crt' )  
  
